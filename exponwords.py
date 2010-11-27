ac867b8bf00c0f792e549ca7c9481b5d1fb62cc9
Csaba Hoch <csaba.hoch@gmail.com>
2010-11-27 20:06:16 +0100

    Rewritten

--- exponwords.py
+++ exponwords.py
@@ -5,221 +5,307 @@ import sys
 import re
 import random
 import datetime
-from hcs_args import args_to_od
+import optparse
 
-def print_help(subject = None):
-    print """\
-NAME
-    Words
-USAGE
-    Words [options]
-DESCRIPTION
-    The program can store and ask word pairs.
-COMMANDS
-    h, help, -h, --help:
-        This help.
-    a, ask-words
-        Ask the words.
-    f, show-future
-        Show the future.
-OPTIONS
-    -f, --dict-file-name [file]
-        The name of the dictionary file.
-        The default is: /a/docs/hcs/esperanto/w.txt
-        Commands: ask-words, show-future
-    -d, --future-days [days]
-        Set the future days.
-        10 by default.
-        Commands: show-future
-AUTHOR
-    Csaba Hoch <csaba.hoch@gmail.com>"""
+##### getch #####
+
+class _Getch:
+    """Gets a single character from standard input.  Does not echo to the
+    screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+getch = _Getch()
+
+
+##### Word, WordList #####
+
+
+class Word(object):
+
+    def __repr__(self):
+        return ('<%s -- %s, Expl: %s>' %
+                (repr(self.lang1), repr(self.lang2), repr(self.explanation)))
+
+
+class WordList(object):
+
+    def __init__(self):
+        self.list = []
+        self.langs = ['', '']
+
 
 def words_from_file(dict_file_name):
-    file = open(dict_file_name,'r')
-    words = []
+    """Reads a word list from a file.
+
+    Argument:
+    - dict_file_name (str)
+
+    Returns: WordList | None
+    """
+
+    file = open(dict_file_name, 'r')
+    wordlist = WordList()
+    i = 0
     for line in file:
-        line = line.strip()
-        if line != '':
-            r = re.search(r'<(\d+) +(\d\d\d\d)-(\d\d)-(\d\d)> +([^\|]+)\|(.*)',line)
-            if r == None:
-                r = re.search('([^\|]+) *\| *(.*)',line)
-                strength = 0
-                date = datetime.date.today()
-                eo = r.group(1).strip()
-                hu = r.group(2).strip()
+        line = line.rstrip()
+        if line == '':
+            pass
+        elif line[0] == ' ':
+            if len(wordlist.list) > 0:
+                wordlist.list[-1].explanation += line + '\n'
+        elif line.startswith('CONFIG:'):
+            regexp = '^CONFIG: (.*?)=(.*)$'
+            r = re.search(regexp, line)
+            if r is None:
+                print 'ERROR: Line %s is incorrect: %s' % (i, line)
+                return None
+            key = r.group(1)
+            value = r.group(2)
+            if key == 'lang1':
+                wordlist.langs[0] = value
+            elif key == 'lang2':
+                wordlist.langs[1] = value
             else:
-                strength = int(r.group(1))
-                date = datetime.date(int(r.group(2)),int(r.group(3)),int(r.group(4)))
-                eo = r.group(5).strip()
-                hu = r.group(6).strip()
-            words.append((strength,date,eo,hu))
-    file.close()
-    return words
-
-def words_to_file(words,dict_file_name):
-    file = open(dict_file_name,'w')
-    for (strength, date, eo, hu) in words:
-        file.write('<'+str(strength)+' '+str(date)+'> '+eo+' | '+hu+'\n')
-    file.close()
-
-#def new_word():
-#    global esp_word, hun_word, esp_to_hun
-#    esp_word, hun_word = words[random.randint(0,len(words)-1)]
-#    esp_to_hun = True if random.randint(1,2) == 1 else False
-
-def x_to_accent(w):
-    i = 1
-    while i < len(w):
-        if w[i] == 'x':
-            if   w[i-1] == 'c': new = 'ĉ'
-            elif w[i-1] == 'g': new = 'ĝ'
-            elif w[i-1] == 'h': new = 'ĥ'
-            elif w[i-1] == 'j': new = 'ĵ'
-            elif w[i-1] == 's': new = 'ŝ'
-            elif w[i-1] == 'u': new = 'ŭ'
-            elif w[i-1] == 'C': new = 'Ĉ'
-            elif w[i-1] == 'G': new = 'Ĝ'
-            elif w[i-1] == 'H': new = 'Ĥ'
-            elif w[i-1] == 'J': new = 'Ĵ'
-            elif w[i-1] == 'S': new = 'Ŝ'
-            w = w[0:i-1] + new + w[i+1:]
+                print ('ERROR: Unknown configuration key at line %s: %s' %
+                       (i, key))
+                return None
         else:
-            i += 1
-    return w
+            strength_date_regexp = '<(\d+) +(\d\d\d\d)-(\d\d)-(\d\d)>'
+            regexp = ('^(.*?) -- (.*?)' +
+                      '( ' + strength_date_regexp * 2 + ')?' +
+                      '$')
+            r = re.search(regexp, line)
+            if r is None:
+                print 'ERROR: Line %s is incorrect: %s' % (i, line)
+                return None
 
-def ask_word(eo,hu,dir):
-    if dir == 'eh':
-        sys.stdout.write('Esperanta vorto: %s = ' % x_to_accent(eo))
-        solution = hu
+            word = Word()
+            word.langs = [r.group(1), r.group(2)]
+            if r.group(3) is not None:
+                word.strengths = [int(r.group(4)), int(r.group(8))]
+                word.dates = [datetime.date(int(r.group(5)),
+                                           int(r.group(6)),
+                                           int(r.group(7))),
+                              datetime.date(int(r.group(9)),
+                                           int(r.group(10)),
+                                           int(r.group(11)))]
+            else:
+                word.strengths = [0, 0]
+                word.dates = [datetime.date.today(), datetime.date.today()]
+            word.explanation = ''
+            wordlist.list.append(word)
+        i += 1
+    file.close()
+    return wordlist
+
+
+def words_to_file(wordlist, dict_file_name):
+    """Writes the word list to a file.
+
+    Arguments:
+    - words (WordList)
+    - dict_file_name (str)
+    """
+
+    file = open(dict_file_name, 'w')
+    file.write('CONFIG: lang1=%s\n' % wordlist.langs[0])
+    file.write('CONFIG: lang2=%s\n' % wordlist.langs[1])
+    for word in wordlist.list:
+        file.write(
+            '%s -- %s <%s %s><%s %s>\n%s' %
+            (word.langs[0],
+             word.langs[1],
+             str(word.strengths[0]),
+             str(word.dates[0]),
+             str(word.strengths[1]),
+             str(word.dates[1]),
+             word.explanation))
+    file.close()
+
+
+def ask_word(word, direction, wordlist, use_getch=False):
+    """Asks a word from the user via the console.
+
+    Arguments:
+    - word (Word)
+    - direction (int) -- 0 or 1.
+    - use_getch (bool)
+
+    Returns: bool | 'quit'
+    """
+
+    from_lang = wordlist.langs[direction]
+    question_word = word.langs[direction]
+    solution_word = word.langs[1 - direction]
+
+    sys.stdout.write('Word in %s: %s -- ' % (from_lang, question_word))
+    if use_getch:
+        ch = getch()
     else:
-        sys.stdout.write('Hungara vorto: %s = ' % hu)
-        solution = eo
-    r = re.search('([^ ,]+).*',solution)
-    solution = r.group(1)
-    line = sys.stdin.readline().strip()
-    if line == 'q':
-        return 'q'
-    elif line == solution:
-        sys.stdout.write('++++++++++++++++++++\n')
-        return True
-    else:
-        sys.stdout.write( \
-                '-------------------- '+ \
-                ('Malĝusta; ' if solution!='' else '') + \
-                'la solvaĝo estas: ' + \
-                solution + '\n')
-        if line != '':
-            return (True if sys.stdin.readline().strip() == 'c' else False)
+        ch = sys.stdin.readline().strip()
+
+    if ch == 'q':
+        return 'quit'
+    print solution_word
+    sys.stdout.write(word.explanation)
+
+    while True:
+        print 'Did you know this word? [y/n/q]'
+        if use_getch:
+            ch = getch()
         else:
+            ch = sys.stdin.readline().strip()
+        if ch == 'q':
+            return 'quit'
+        elif ch == 'y':
+            return True
+        elif ch == 'n':
             return False
 
-def next_date(strength,date):
-    return date + datetime.timedelta(2**strength)
 
-def practice_words(words):
+def next_date(strength, date):
+    return date + datetime.timedelta(2 ** strength)
+
+
+def practice_words(wordlist, use_getch=False):
+    """Practice the words that need to be asked.
+
+    Argument:
+    - wordlist (WordList)
+    - use_getch (bool)
+    """
+
     today = datetime.date.today()
-    words_done = []
     words_to_do = []
-    for word in words:
-        strength,date,eo,hu = word
-        if date <= today:
-            words_to_do.append((strength,date,eo,hu,'eh'))
-            words_to_do.append((strength,date,eo,hu,'he'))
-        else:
-            words_done.append(word)
+    for word in wordlist.list:
+        for direction in [0, 1]:
+            if word.dates[direction] <= today:
+                words_to_do.append((word, direction))
     random.shuffle(words_to_do)
 
-    sys.stdout.write("%d vortoj estos demanita.\n" % len(words_to_do))
-    sys.stdout.write("Proksimuma tempo en minutoj: %d\n" % (len(words_to_do)/14))
+    print 'Total number of words: %s' % (len(wordlist.list) * 2)
+    print 'Words to ask now: %s' % (len(words_to_do))
 
-    t = {} # t(eo,hu) = 0: failed; t(eo,hu) = 1: one passed
-    finished = False
     i = 0
-    for strength,date,eo,hu,dir in words_to_do:
-        if finished:
-            if (eo,hu) not in s:
-                words_done.append((strength,date,eo,hu))
-                s.add((eo,hu))
-        else:
-            i += 1
-            sys.stdout.write(str(len(words_to_do))+'/'+str(i)+' <'+str(strength)+'>: ')
-            answer_ok = ask_word(eo,hu,dir)
-            if answer_ok == 'q':
-                s = set()
-                s.add((eo,hu))
-                words_done.append((strength,date,eo,hu))
-                finished = True
-            elif not (eo,hu) in t:
-                t[(eo,hu)] = answer_ok
-            else:
-                if t[(eo,hu)] and answer_ok:
-                    words_done.append((strength+1,next_date(strength,today),eo,hu))
-                else:
-                    words_done.append((0,today,eo,hu))
-                del t[(eo,hu)]
+    for word, direction in words_to_do:
+        i += 1
+        strength = word.strengths[direction]
+        date = word.dates[direction]
 
-    return words_done
+        sys.stdout.write('%s/%s <%s>: ' % (len(words_to_do), i, strength))
 
-def inc_wd(wd,date,strength,n):
-    if date not in wd:
-        wd[date] = {strength: n}
-    elif strength not in wd[date]:
-        wd[date][strength] = n
+        answer = ask_word(word, direction, wordlist, use_getch)
+        if answer == 'quit':
+            break
+        elif answer is True:
+            word.strengths[direction] += 1
+            word.dates[direction] = next_date(strength, today)
+        elif answer is False:
+            word.strengths[direction] = 0
+            word.dates[direction] = today
+
+
+# def inc_wd(wd,date,strength,n):
+#     if date not in wd:
+#         wd[date] = {strength: n}
+#     elif strength not in wd[date]:
+#         wd[date][strength] = n
+#     else:
+#         wd[date][strength] += n
+#
+# def print_future(words,days):
+#     wd = {} # word dictionary: {date: {strength: words_count}}
+#     for strength,date,eo,hu in words:
+#         inc_wd(wd,date,strength,1)
+#     date = datetime.date.today()
+#     for i in range(days):
+#         sys.stdout.write(str(date)+': ')
+#         if date in wd:
+#             for strength,words_count in wd[date].items():
+#                 sys.stdout.write('*'*words_count)
+#                 inc_wd(wd,next_date(strength,date),strength+1,words_count)
+#             del wd[date]
+#         print
+#         date += datetime.timedelta(1)
+
+
+def ask_words(options):
+    fname = options.dict_file_name
+    wordlist = words_from_file(fname)
+    sys.stdout.write("%d word pairs read.\n" % len(wordlist.list))
+    practice_words(wordlist, use_getch=options.getch)
+    words_to_file(wordlist, fname)
+    if options.backup:
+        words_to_file(wordlist, fname + '_' + str(datetime.date.today()))
+
+# def show_future(od):
+#     words = words_from_file(od['dict-file-name'])
+#     print_future(words,od['future-days'])
+
+def parse_args():
+    """Parses the given command line options.
+
+    Returns: (optparse.Values, [str])
+    """
+
+    parser = optparse.OptionParser()
+
+    parser.add_option('-f', '--dict-file-name', dest='dict_file_name',
+                      help='The name of the dictionary file. The default is: '
+                      '/a/docs/hcs/esperanto/w.txt Commands: ask-words, '
+                      'show-future',
+                      action='store', default=None)
+#    parser.add_option('-d', '--future-days', dest='future_days',
+#                      help='Set the future days. 10 by default. Commands: '
+#                      'show-future',
+#                      type='int', action='store', default=10)
+    parser.add_option('-g', '--getch', dest='getch',
+                      help='Use getch() instead of readline()',
+                      action='store_true')
+    parser.add_option('-b', '--backup', dest='backup',
+                      help='Create backup files',
+                      action='store_true')
+    (options, args) = parser.parse_args()
+    return (options, args)
+
+
+def main(options, args):
+    if args in [[], ['ask-words'], ['a']]:
+        ask_words(options)
+    elif args in [['show-future'], ['f']]:
+        show_future(options)
     else:
-        wd[date][strength] += n
+        print "Unknown command: '%s'" % args
 
-def print_future(words,days):
-    wd = {} # word dictionary: {date: {strength: words_count}}
-    for strength,date,eo,hu in words:
-        inc_wd(wd,date,strength,1)
-    date = datetime.date.today()
-    for i in range(days):
-        sys.stdout.write(str(date)+': ')
-        if date in wd:
-            for strength,words_count in wd[date].items():
-                sys.stdout.write('*'*words_count)
-                inc_wd(wd,next_date(strength,date),strength+1,words_count)
-            del wd[date]
-        print
-        date += datetime.timedelta(1)
-
-def ask_words(od):
-    dict_file_name = od['dict-file-name']
-    words = words_from_file(dict_file_name)
-    sys.stdout.write("%d vortparoj entute.\n" % len(words))
-    words = practice_words(words)
-    words_to_file(words,dict_file_name)
-    words_to_file(words,dict_file_name+'_'+str(datetime.date.today()))
-
-def show_future(od):
-    words = words_from_file(od['dict-file-name'])
-    print_future(words,od['future-days'])
-
-def main(args):
-    cmd = ('help' if len(args) == 0 else args.pop(0))
-    dict_file_name_option = ['dict-file-name','f', 1, '/a/docs/hcs/esperanto/w.txt']
-    if cmd in ['help','h','--help','-h']:
-        print_help(None if len(args) == 0 else args.pop(0))
-        return
-    elif cmd in ['ask-words','a']:
-        opt_spec = [dict_file_name_option]
-        ask_words(args_to_od(args, opt_spec))
-    elif cmd in ['show-future','f']:
-        opt_spec = [dict_file_name_option,
-                    ['future-days', 'd', 1, 10]]
-        od = args_to_od(args, opt_spec)
-        try:
-            od['future-days'] = int(od['future-days'])
-        except ValueError:
-            print "Error: integer expected after 'future-days' option, but '%s'" \
-                  " is not an integer." % (od['future-days'],)
-            return
-        show_future(od)
-    else:
-        raise Exception, "Unknown command: '%s'" % cmd
 
 if __name__ == '__main__':
-    try:
-        main(sys.argv[1:])
-    except Exception, e:
-        print "Error:", e
+    main(*parse_args())
