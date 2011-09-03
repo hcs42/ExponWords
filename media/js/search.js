e92fe94ba7780e9446c986a3cf8e8b26caa5a2cc
Csaba Hoch <csaba.hoch@gmail.com>
2011-09-03 08:54:20 +0200

    Clicking on the row selects the word pair
    If the user clicks anywhere in the row in the table of the search hits
    on the search page (except for links), then the word pair in the row will be
    selected.
    
    The selection is visualized with gray background.

--- media/js/search.js
+++ media/js/search.js
@@ -20,21 +20,22 @@ var select_all_text;
 var select_none_text;
 
 function select_all() {
-    var checkboxes = $('#table input[type=checkbox]');
     if (all_selected) {
         // Select none
-        checkboxes.attr('checked', false);
-        all_selected = false;
+        $('#hits :checkbox').attr('checked', false);
+        $('#hits tr').filter(':has(:checkbox)').removeClass('selected');
         $('#select-all-button').text(select_all_text);
+        all_selected = false;
     } else {
         // Select all
-        checkboxes.attr('checked', true);
-        all_selected = true;
+        $('#hits :checkbox').attr('checked', true);
+        $('#hits tr').filter(':has(:checkbox)').addClass('selected');
         $('#select-all-button').text(select_none_text);
+        all_selected = true;
     }
 }
 
-function selection_changed() {
+function action_selection_changed() {
     var value = $('#action-selection').val();
     var spans = $('span[id^="span-"]');
     $.each(spans, function() {
@@ -50,8 +51,24 @@ $(document).ready(function() {
 
     // Event handlers
     $('#select-all-button').click(select_all);
-    $('#action-selection').change(selection_changed);
+    $('#action-selection').change(action_selection_changed);
 
     select_all_text = $("#select-all-button").text();
     select_none_text = $("#translation-select-none").text();
+
+    // The following code is based on: http://www.learningjquery.com/2008/12/quick-tip-click-table-row-to-trigger-a-checkbox-click
+    $('#hits tr')
+    .filter(':has(:checkbox:checked)')
+    .addClass('selected')
+    .end()
+    .click(function(e) {
+        // Don't do anything if the user clicked on a link a cell.
+        if (!$(e.target).is('a')) {
+            if (e.target.type === 'checkbox') {
+                $(this).toggleClass('selected');
+            } else {
+                $(':checkbox', this).trigger('click');
+            }
+        }
+    });
 });
