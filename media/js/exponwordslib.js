fcbe9aad6e0be564674e887d11cbf34a378f450f
Csaba Hoch <csaba.hoch@gmail.com>
2012-11-24 09:46:04 +0100

    exponwordslib.js: unused translation fw removed

--- media/js/exponwordslib.js
+++ media/js/exponwordslib.js
@@ -16,38 +16,6 @@
 // Copyright (C) 2011-2012 Csaba Hoch
 
 
-// Translation of the user interface
-var tr_dict;
-
-function tr(original) {
-    if (original in tr_dict) {
-        return tr_dict[original];
-    } else {
-        return original;
-    }
-}
-
-function translate_node_text(node)
-{
-    node.text(tr(node.text()));
-}
-
-function get_translation(callback) {
-
-    // Get the translation and call the callback function that will translate
-    // the UI
-    $.ajax({
-        url: '/get_translation',
-        dataType: 'json',
-        data: {},
-        type: 'post',
-        success: function(result) {
-            tr_dict = result;
-            callback();
-        }
-    });
-}
-
 function ew_strip(str) {
     return str.replace(/^\s+/, '').replace(/\s+$/, '');
 }
