hcs42/ExponWords

commit 8abedcd6cc6a217a62a903ed7dc0f7a0b696caab

Word pair page: span -> button
Buttons are better here because they are not broken into two lines by the
mobile browsers.

templates/ew/word_pair.html
@@ -72,10 +72,10 @@
         </div>
         {% endif %}
 
-        <span id="advanced_button" class="nice-button" style="{{ simple_style }}">
+        <button id="advanced_button" class="nice-button" style="{{ simple_style }}">
           {% trans 'Advanced view' %}
         </span>
-        <span id="simple_button" class="nice-button" style="{{ advanced_style }}">
+        <button id="simple_button" class="nice-button" style="{{ advanced_style }}">
           {% trans 'Simple view' %}
         </span>
       </td>

commit c68d6ec965c5ffb761d944924bbdcbfe5583ad94

Bugfix: wp was added after using the view buttons
A bug was introduced in commit 8abedcd6cc6a217a62a903ed7dc0f7a0b696caab to the
"Add new word pair" page, because it changed the view buttons ("Simple
view"/"Advanced view") from "span" to "button". Since (at least in Chromium)
the default button type is "submit", the "add word pair" HTML form was
submitted when these buttons were pressed, which caused the word pair to be
added.

models.py
@@ -24,7 +24,7 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 
 
-version = '0.13.1'
+version = '0.13.2'
 
 ##### Constants #####
 

templates/ew/practice_wdict.html
@@ -46,7 +46,7 @@
   (<span id="answered-incorrectly"></span>) /
   <span id="all-now"></span>
   <span id="all-today"></span>
-  <button id="show-operations-button" class="nice-button">{% trans 'More' %}</button>
+  <button type="button" id="show-operations-button" class="nice-button">{% trans 'More' %}</button>
   <div id="operations" class="practice-operations" style="display:none;">
     <p>{% trans "Operations" %}:
     <span id="hide-operations-button" class="nice-button">{% trans 'Hide' %}</span>
@@ -65,9 +65,9 @@
 <div id="main">
   {% if ewuser.practice_arrangement == 'normal' %}
 <div id="buttons">
-  <button class="button" style="display:none;font-size:{{ ewuser.button_size }}" id="yes-button">{% trans "YES" %}</button>
-  <button class="button" style="display:none;font-size:{{ ewuser.button_size }}" id="no-button">{% trans "NO" %}</button>
-  <button class="button" style="display:none;font-size:{{ ewuser.button_size }}" id="ok-button">{% trans "ANSWER" %}</button>
+  <button type="button" class="button" style="display:none;font-size:{{ ewuser.button_size }}" id="yes-button">{% trans "YES" %}</button>
+  <button type="button" class="button" style="display:none;font-size:{{ ewuser.button_size }}" id="no-button">{% trans "NO" %}</button>
+  <button type="button" class="button" style="display:none;font-size:{{ ewuser.button_size }}" id="ok-button">{% trans "ANSWER" %}</button>
 </div>
 <div id="question" style="font-size:{{ ewuser.question_size }}"></div>
 <div id="answer" style="font-size:{{ ewuser.answer_size }}"></div>

templates/ew/word_pair.html
@@ -72,10 +72,10 @@
         </div>
         {% endif %}
 
-        <button id="advanced_button" class="nice-button" style="{{ simple_style }}">
+        <button id="advanced_button" type="button" class="nice-button" style="{{ simple_style }}">
           {% trans 'Advanced view' %}
         </span>
-        <button id="simple_button" class="nice-button" style="{{ advanced_style }}">
+        <button id="simple_button" type="button" class="nice-button" style="{{ advanced_style }}">
           {% trans 'Simple view' %}
         </span>
       </td>